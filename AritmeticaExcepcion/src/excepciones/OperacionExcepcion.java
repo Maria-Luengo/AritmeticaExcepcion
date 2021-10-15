a3448d313ac51ede878007ae91d696ecec5d7fa2
Maria-Luengo <77382852+Maria-Luengo@users.noreply.github.com>
2021-10-15 11:52:46 +0200

    Update OperacionExcepcion.java

--- AritmeticaExcepcion/src/excepciones/OperacionExcepcion.java
+++ AritmeticaExcepcion/src/excepciones/OperacionExcepcion.java
@@ -2,6 +2,8 @@ package excepciones;
 
 //OJO extiende de la clase Exception
 public class OperacionExcepcion extends Exception{
+    /*SIEMPRE DEBEN SER MANAJADAS, (tener un bloque try catch)
+    HAy otro bloque Try-Catch-Finally*/
 
     public OperacionExcepcion(String mensaje) {
         super(mensaje);
