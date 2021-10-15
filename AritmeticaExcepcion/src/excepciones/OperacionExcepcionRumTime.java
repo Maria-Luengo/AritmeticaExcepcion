8d9a33475abedd1069fe1d1cf6e2c9b22e3f45fc
Maria-Luengo <77382852+Maria-Luengo@users.noreply.github.com>
2021-10-15 11:52:44 +0200

    Create OperacionExcepcionRumTime.java

---
+++ AritmeticaExcepcion/src/excepciones/OperacionExcepcionRumTime.java
@@ -0,0 +1,9 @@
+package excepciones;
+
+//NO TENGO OBLIGATORIEDAD DE HACER Try Catch
+public class OperacionExcepcionRumTime extends Exception{
+    
+    public OperacionExcepcionRumTime(String mensaje) {
+        super(mensaje);
+    }
+}
